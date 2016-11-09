34e7001a082e780bf9df0ca322828c1426069163
Pradeep Kumar R <pradeepraghu9427@gmail.com>
2016-11-09 12:55:44 +0530

    Add game with one error

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+this.x=x;
+this.y=y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,23 +15,81 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+  //  console.log('hello');
+    //MOVEMENT OF ENEMY
+    this.speed = Math.random()*200 +50;
+    if (this.x<500){
+    this.x = this.x + this.speed * dt;
+}
+  };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    if(this.x>500) {
+      this.x=0;
+      ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
+    }
 };
 
+var Player = function (x,y) {
+  this.x =x;
+  this.y=y;
+  this.sprite ='images/char-boy.png';
+};
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+Player.prototype.update = function() {
+  //console.log('player');
+if (this.y === 0){
+  this.y = 400;
+}
+};
+
+Player.prototype.render = function() {
+  //console.log('player-render');
+ctx.drawImage(Resources.get(this.sprite), this.x,this.y);
+
+};
+
+Player.prototype.handleInput = function(keys) {
+  var posy = 70;
+  var posx = 100;
+if (keys === 'left' && this.x>0 ) {
+  this.x= this.x-posx;
+}else if (keys==='up' && this.y>0) {
+  this.y= this.y-posy;
+}else if(keys ==='right' && this.x<400) {
+  this.x= this.x + posx;
+}else if(keys === 'down' && this.y<350) {
+  this.y = this.y + posy;
+}
+};
+
+Player.prototype.collision = function (x,y) {
+  var Bx =x;
+  var By =y;
+  Bwidth=60;
+  Bheight=80;
+  Pwidth=50;
+  Pheight=70;
+
+  if (this.x < Bx + Bwidth &&
+   this.x + Pwidth >Bx &&
+   this.y < By + Bheight &&
+   Pheight + this.y > By) {
+    // collision detected!
+    this.y=375;
+}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [new Enemy(30,50),new Enemy(30,150),new Enemy(30,250)];
+var player=new Player(200,400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -80,7 +80,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+         checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -90,6 +90,13 @@ var Engine = (function(global) {
      * the data/properties related to the object. Do your drawing in your
      * render methods.
      */
+
+     function checkCollisions(){
+       allEnemies.forEach(function(enemy) {
+         player.collision(enemy.x,enemy.y);
+       });
+     }
+
     function updateEntities(dt) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
